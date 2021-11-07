ddf03ef760eb5b724cf568fecbe4cf4749d0d494
Vitaliy <starozhil2008@mail.ru>
2021-11-07 14:38:54 +0300

    change getMinMaxOfArray

--- src/ThirdHomeWork.java
+++ src/ThirdHomeWork.java
@@ -135,7 +135,7 @@ class ThirdHomeWork {
             int min = arr[0], max = arr[0];
             for (int i = 0; i < arr.length; i++) {
                 if (arr[i] > max) max = arr[i];
-                if (arr[i] < min) max = arr[i];
+                if (arr[i] < min) min = arr[i];
             }
             System.out.println("min = "+min+", max = "+max);
         }
